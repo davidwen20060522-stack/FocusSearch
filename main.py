8692072d06e18ef50920c84ad8a54b423ed15c71
davidwen20060522-stack <davidwen20060522@gmail.com>
2026-09-22 22:16:50 +0800

    Release FocusSearch v0.2.0
    Add Xiaohongshu, Bilibili, Baidu and ALL search modes, input validation, repeat-search flow, updated documentation, and changelog.

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def welcome_page():
     return search_url
 
 def choose_page(search):
+
+
     print("========================================")
     print("              选择搜索平台")
     print("========================================")
@@ -23,16 +25,40 @@ def choose_page(search):
     print("          搜索：" + unquote(search))
     print(" ")
     print("          [1] 抖音")
+    print("          [2] 小红书")
+    print("          [3] b站")
+    print("          [4] 百度")
+    print("          [5] ALL")
     print(" ")
     print("----------------------------------------")
     n = input(">")
     if n == "1":
         searchway.Douyin(search)
+    elif n == "2":
+        searchway.Xiaohongshu(search)
+    elif n == "3":
+        searchway.Bilibili(search)
+    elif n == "4":
+        searchway.Baidu(search)
+    elif n == "5":
+        searchway.Douyin(search)
+        searchway.Xiaohongshu(search)
+        searchway.Bilibili(search)
+        searchway.Baidu(search)
+    else:
+        print("选择有误！！仔细看！")
+        value = "error"
+        return value
+
     os.system("cls")
     print("========================================")
     print("              Finish!!!")
     print("========================================")
     input()
 
-search = welcome_page()
-choose_page(search)
+value = "1"
+while True:
+    search = welcome_page()
+    value = choose_page(search)
+    if (value == "error"):
+        break
--- searchway.py
+++ searchway.py
@@ -3,5 +3,19 @@ import webbrowser
 def Douyin(search):
     url_douyin = "https://www.douyin.com/search/"
     url_obj1 = url_douyin + search
-    print("1")
     webbrowser.open_new(url_obj1)
+
+def Xiaohongshu(search):
+    url_xiaohongshu = "https://www.xiaohongshu.com/search_result?keyword="
+    url_obj2 = url_xiaohongshu + search
+    webbrowser.open_new(url_obj2)
+
+def Bilibili(search):
+    url_bilibili = "https://search.bilibili.com/all?keyword="
+    url_obj3 = url_bilibili + search
+    webbrowser.open_new(url_obj3)
+
+def Baidu(search):
+    url_baidu = "https://wap.baidu.com/ssid=400c42616c6c6f7761676e6973g532601000000/s?word="
+    url_obj4 = url_baidu + search
+    webbrowser.open_new(url_obj4)
